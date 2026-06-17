c302c6cda117cd5a6cd7c4d73218233963aa3698
Venkata Naga Kusal <kottekvnkusal@gmail.com>
2026-06-17 12:28:05 +0530

    added header,intro of page-1

---
+++ data.js
@@ -0,0 +1,28 @@
+export const blogData = [
+    {
+        name:'blog-1',
+        image:'article-image-03',
+        date:'JULY 23,2022',
+        content:`I'm excited to start a new learning journey
+         as a Scrimba Bootcamp student! 
+         After several months of learning in the 
+         Frontend Developer Career Path.`
+    },
+    {
+        name:'blog-2',
+        image:'article-image-02',
+        date:'JULY 23,2022',
+        content:`I'm excited to start a new learning 
+        journey as a Scrimba Bootcamp student! 
+        After several months of learning in the 
+        Frontend Developer Career Path.`
+    },
+    {
+        name:'blog-3',
+        image:'article-image-01',
+        date:'JULY 23,2022',
+        content:`I'm excited to start a new learning journey 
+        as a Scrimba Bootcamp student! After several months 
+        of learning in the Frontend Developer Career Path.`
+    }
+]
